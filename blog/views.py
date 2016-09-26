f49807f0d09ff09eba7bdea4e1ab3da073f444a9
Hanju Jo <gkswn121@gmail.com>
2016-09-26 10:55:22 +0900

    Fix bugs

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -22,7 +23,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-        return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
